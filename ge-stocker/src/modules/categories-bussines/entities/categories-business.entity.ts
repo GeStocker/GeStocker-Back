fd848cbdd8f59d3f25fd2d32ee59129d7dc53e16
Buffalopeter <pedroleone95@gmail.com>
2025-03-17 17:02:13 -0300

    categories-business.entity, business.entity, categories-product.entity, product.entity: categories done, relations with each entity done

--- ge-stocker/src/modules/categories-bussines/entities/categories-business.entity.ts
+++ ge-stocker/src/modules/categories-bussines/entities/categories-business.entity.ts
@@ -1,1 +1,20 @@
-export class CategoriesBusiness {}
+import { Bussines } from "src/modules/bussines/entities/bussines.entity";
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity ({
+    name: 'CATEGORIES_BUSINESS'
+})
+export class CategoriesBusiness {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column({
+        type: 'varchar',
+        length: 50,
+        nullable: false,
+    })
+    name: string;
+
+    @OneToMany(() => Bussines, (business) => business.category)
+    business: Bussines;
+}
