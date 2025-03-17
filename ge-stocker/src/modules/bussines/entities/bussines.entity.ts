f102f922563badbb86e8e8388d83ac01b6627e8a
GregorioVilla <GregoVilla02@gmail.com>
2025-03-17 16:23:47 -0300

    arreglos

--- ge-stocker/src/modules/bussines/entities/bussines.entity.ts
+++ ge-stocker/src/modules/bussines/entities/bussines.entity.ts
@@ -1,11 +1,1 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
-
-@Entity()
-export class Bussines {
-  @PrimaryGeneratedColumn('uuid')
-  id: string;
-  @Column()
-  name: string;
-  @Column()
-  Direccion: string;
-}
+export class Bussines {}
