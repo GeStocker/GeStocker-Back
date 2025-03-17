0f9300e0efa05f0fed2327e9aaaf20e11015bd84
GregorioVilla <GregoVilla02@gmail.com>
2025-03-17 20:42:09 -0300

    BDD Correcta

--- ge-stocker/src/config/typeORM.config.ts
+++ ge-stocker/src/config/typeORM.config.ts
@@ -1,5 +1,12 @@
 import { ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Bussines } from 'src/modules/bussines/entities/bussines.entity';
+import { CategoriesBusiness } from 'src/modules/categories-bussines/entities/categories-business.entity';
+import { CategoriesProduct } from 'src/modules/categories-product/entities/categories-product.entity';
+import { Inventory } from 'src/modules/inventory/entities/inventory.entity';
+import { Product } from 'src/modules/products/entities/product.entity';
+import { Role } from 'src/modules/roles/entities/role.entity';
+import { User } from 'src/modules/users/entities/user.entity';
 
 export const TypeOrmConfig = TypeOrmModule.forRootAsync({
     inject: [ConfigService],
@@ -10,7 +17,7 @@ export const TypeOrmConfig = TypeOrmModule.forRootAsync({
         username: configService.get('DB_USER'),
         password: configService.get('DB_PASSWORD'),
         database: configService.get('DB_NAME'),
-        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        entities: [User, Role, Bussines, Inventory, Product, CategoriesProduct, CategoriesBusiness],
         synchronize: true,
         logging: false,
         dropSchema: true,
