bb5afc655a3fe668a8892e7bbcd15d1c12b60c84
GregorioVilla <GregoVilla02@gmail.com>
2025-03-17 15:22:21 -0300

    Entity sin relaciones

--- ge-stocker/src/modules/inventory/entities/inventory.entity.ts
+++ ge-stocker/src/modules/inventory/entities/inventory.entity.ts
@@ -1,1 +1,17 @@
-export class Inventory {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Inventory {
+    @PrimaryGeneratedColumn("uuid")
+    id: string;
+
+    @Column()
+    name: string;
+    
+    @Column()
+    description: string;
+
+    @Column()
+    fechaDeCreacion: Date;
+
+}
