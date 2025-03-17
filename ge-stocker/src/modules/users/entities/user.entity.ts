9788eb4f4e2de2bd25882454ad4ce279b522261a
GregorioVilla <GregoVilla02@gmail.com>
2025-03-17 16:43:10 -0300

    Realciones INV/BUS-USUSARIO

--- ge-stocker/src/modules/users/entities/user.entity.ts
+++ ge-stocker/src/modules/users/entities/user.entity.ts
@@ -1,65 +1,81 @@
-import { Column, Entity, PrimaryGeneratedColumn, CreateDateColumn, ManyToOne, JoinColumn } from "typeorm";
-import { Role } from "src/modules/roles/entities/role.entity";
+import {
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  ManyToOne,
+  JoinColumn,
+  OneToMany,
+} from 'typeorm';
+import { Role } from 'src/modules/roles/entities/role.entity';
+import { Bussines } from 'src/modules/bussines/entities/bussines.entity';
+import { Inventory } from 'src/modules/inventory/entities/inventory.entity';
 
 @Entity({
-    name: 'users'
+  name: 'users',
 })
 export class User {
-    @PrimaryGeneratedColumn('uuid')
-    id: string;
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
-    @Column(
-        {
-            type: 'varchar',
-            length: 50,
-            nullable: false,
-            unique: true
-        }
-    )
-    email: string;
+  @Column({
+    type: 'varchar',
+    length: 50,
+    nullable: false,
+    unique: true,
+  })
+  email: string;
 
-    @Column({
-        type: 'varchar',
-        length: 50,
-        nullable: false
-    })
-    name: string;
+  @Column({
+    type: 'varchar',
+    length: 50,
+    nullable: false,
+  })
+  name: string;
 
-    @Column({
-        type: 'varchar',
-        length: 100,
-        nullable: false
-    })
-    password: string;
+  @Column({
+    type: 'varchar',
+    length: 100,
+    nullable: false,
+  })
+  password: string;
 
-    @Column({
-        type: 'bigint'
-    })
-    phone: number;
+  @Column({
+    type: 'bigint',
+  })
+  phone: number;
 
-    @Column({
-        type: 'varchar',
-        length: 50,
-        nullable: true
-    })
-    country?: string;
+  @Column({
+    type: 'varchar',
+    length: 50,
+    nullable: true,
+  })
+  country?: string;
 
-    @Column({
-        type: 'varchar',
-        length: 50,
-        nullable: true
-    })
-    city?: string;
+  @Column({
+    type: 'varchar',
+    length: 50,
+    nullable: true,
+  })
+  city?: string;
 
-    @Column({
-        type: 'text'
-    })
-    address: string;
+  @Column({
+    type: 'text',
+  })
+  address: string;
 
-    @CreateDateColumn()
-    createdAt: string;
+  @CreateDateColumn()
+  createdAt: string;
 
-    @ManyToOne(() => Role)
-    @JoinColumn({ name: 'role' })
-    role: Role;
-} 
+  @ManyToOne(() => Role)
+  @JoinColumn({ name: 'role' })
+  role: Role;
+
+  @OneToMany(() => Bussines, (business) => business.user)
+  @JoinColumn({ name: 'buisiness_id' })
+  businesses: Bussines[];
+
+  @OneToMany(() => Inventory, (inventory) => inventory.user)
+  @JoinColumn({ name: 'inventory_id' })
+  inventories: Inventory[];
+}
