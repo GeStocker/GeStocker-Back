893080198f5497fbf1a7672afc4b75f443e8f28f
Buffalopeter <pedroleone95@gmail.com>
2025-03-18 18:16:57 -0300

    bussines: createBusiness DTO, controller and service

--- ge-stocker/src/modules/bussines/dto/update-bussine.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateBussinesDto } from 'src/modules/bussines/dto/create-bussine.dto';
-
-export class UpdateBussinesDto extends PartialType(CreateBussinesDto) {}
