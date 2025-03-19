e05317cd380b1e168bd4b963f17de222d949ee94
Buffalopeter <pedroleone95@gmail.com>
2025-03-19 18:56:57 -0300

    CustomRequest fixed

--- ge-stocker/src/interfaces/custom-request.interface.ts
+++ ge-stocker/src/interfaces/custom-request.interface.ts
@@ -2,5 +2,5 @@ import { Request } from "express";
 import { UserRole } from "src/modules/roles/dto/create-role.dto";
 
 export interface CustomRequest extends Request {
-    user: { id: string, email: string, role?: UserRole[], iat?: number, exp?: number}
+    user: { id: string, email: string, roles?: UserRole[], iat?: number, exp?: number}
 }
