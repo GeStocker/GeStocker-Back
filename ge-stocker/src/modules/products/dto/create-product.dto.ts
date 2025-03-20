feb287e9630d468870f77f35e9c469c348e1944f
Matias Joel Torres <matias.jtor@gmail.com>
2025-03-20 19:29:45 -0300

    Modulo users

--- ge-stocker/src/modules/products/dto/create-product.dto.ts
+++ ge-stocker/src/modules/products/dto/create-product.dto.ts
@@ -22,6 +22,7 @@ export class CreateProductDto {
   img?: string;
 
   @IsNotEmpty()
+  @IsString()
   category: string;
 
   @IsNotEmpty()
--- ge-stocker/src/modules/products/products.service.ts
+++ ge-stocker/src/modules/products/products.service.ts
@@ -1,12 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CategoriesProduct } from '../categories-product/entities/categories-product.entity';
+import { Repository } from 'typeorm';
+import { preload } from 'src/helpers/preload';
+import { Product } from './entities/product.entity';
 
 @Injectable()
 export class ProductsService {
   createProduct(createProductDto: CreateProductDto, userId: string) {
     return 'This action adds a new product';
   }
+
 
   findAll() {
     return `This action returns all products`;
