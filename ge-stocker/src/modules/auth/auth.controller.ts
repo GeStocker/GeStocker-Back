dfd52e6b0e8a728b713afcd7ab40a6f4ca45d500
Tomas36M <tomasmunevar36@gmail.com>
2025-03-20 18:23:15 -0500

    feature/auth craecion de signup con google

--- ge-stocker/src/modules/auth/auth.controller.ts
+++ ge-stocker/src/modules/auth/auth.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get, Req, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateAuthDto, LoginAuthDto } from './dto/create-auth.dto';
+import { GoogleAuthGuard } from './authGoogle.guard';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService) { }
 
   @Post('/signup')
   create(@Body() user: CreateAuthDto) {
@@ -13,5 +14,17 @@ export class AuthController {
   @Post('/login')
   login(@Body() user: LoginAuthDto) {
     return this.authService.login(user);
+  }
+
+  @Get('google')
+  @UseGuards(GoogleAuthGuard)
+  async googleAuth(@Req() req) {}
+
+  @Get('google/callback')
+  @UseGuards(GoogleAuthGuard)
+  async googleAuthRedirect(@Req() req) {
+    const profile = req.user;
+    const user = await this.authService.registerOrUpdateGoogleUser(profile);
+    return user;
   }
 }
--- ge-stocker/src/modules/auth/auth.module.ts
+++ ge-stocker/src/modules/auth/auth.module.ts
@@ -1,13 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { TypeOrmConfig } from 'src/config/typeORM.config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../users/entities/user.entity';
+import { PassportModule } from '@nestjs/passport';
+import { GoogleStrategy } from './authGoogle.startegy';
 
 @Module({
-  imports:[TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    PassportModule.register({ defaultStrategy: 'google' }), 
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, GoogleStrategy],
 })
 export class AuthModule {}
