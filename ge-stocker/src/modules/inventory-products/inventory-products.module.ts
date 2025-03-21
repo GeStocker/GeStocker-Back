a88ecab10bf3cc76174ae5512ceeb6086b0bddc7
Buffalopeter <pedroleone95@gmail.com>
2025-03-21 14:24:50 -0300

    inventory-products: created CRUD. Post, Get and Put for Price. Soft delete managed from Business Products

--- ge-stocker/src/modules/inventory-products/inventory-products.module.ts
+++ ge-stocker/src/modules/inventory-products/inventory-products.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { InventoryProductsService } from './inventory-products.service';
 import { InventoryProductsController } from './inventory-products.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { InventoryProduct } from './entities/inventory-products.entity';
+import { Inventory } from '../inventory/entities/inventory.entity';
+import { Product } from '../products/entities/product.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([InventoryProduct, Inventory, Product])],
   controllers: [InventoryProductsController],
   providers: [InventoryProductsService],
 })
