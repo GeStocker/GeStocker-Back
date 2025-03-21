168d8746f3d28744446c3c35031bab4b114f649c
Buffalopeter <pedroleone95@gmail.com>
2025-03-20 21:06:51 -0300

    products, categories, business: Little fixes in business, created get categories, created Products CRUD

--- ge-stocker/src/modules/products/products.module.ts
+++ ge-stocker/src/modules/products/products.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Business } from '../bussines/entities/bussines.entity';
+import { CategoriesProduct } from '../categories-product/entities/categories-product.entity';
+import { FilesService } from '../files/files.service';
+import { Product } from './entities/product.entity';
+import { CloudinaryRepository } from '../files/files.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Product,Business, CategoriesProduct])],
   controllers: [ProductsController],
-  providers: [ProductsService],
+  providers: [ProductsService, FilesService, CloudinaryRepository],
 })
 export class ProductsModule {}
--- ge-stocker/src/modules/products/products.service.ts
+++ ge-stocker/src/modules/products/products.service.ts
@@ -1,26 +1,123 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Product } from './entities/product.entity';
+import { Not, Repository } from 'typeorm';
+import { Business } from '../bussines/entities/bussines.entity';
+import { CategoriesProduct } from '../categories-product/entities/categories-product.entity';
+import { FilesService } from '../files/files.service';
 
 @Injectable()
 export class ProductsService {
-  createProduct(createProductDto: CreateProductDto, userId: string) {
-    return 'This action adds a new product';
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+    @InjectRepository(Business)
+    private readonly businessRepository: Repository<Business>,
+    @InjectRepository(CategoriesProduct)
+    private readonly categoriesProductRepository: Repository<CategoriesProduct>,
+    private readonly cloudinaryService: FilesService
+  ) {}
+  async createProduct(createProductDto: CreateProductDto, userId: string, file?: Express.Multer.File) {
+    const { name, description, category, businessId } = createProductDto;
+
+    const business = await this.businessRepository.findOne({
+      where: {id: businessId, user: {id: userId } },
+      relations: ['categories'],
+    });
+
+    if (!business) throw new NotFoundException('Business not found!');
+
+    const productExistance = await this.productRepository.findOne({
+      where: { name }
+    });
+
+    if (productExistance) throw new ConflictException('Product already exists!');
+
+    let categoryEntity = await this.categoriesProductRepository.findOne({
+      where: { name: category, business: { id: businessId } },
+    });
+
+    if (!categoryEntity) {
+      categoryEntity = this.categoriesProductRepository.create({ name: category, business });
+      await this.categoriesProductRepository.save(categoryEntity);
+    }
+
+    let imgUrl: string | undefined;
+    if(file) {
+      const uploadResult = await this.cloudinaryService.uploadProductImage(file);
+      imgUrl = uploadResult.secure_url;
+    };
+
+    const newProduct = this.productRepository.create({
+      name,
+      description,
+      img: imgUrl,
+      business,
+      category: categoryEntity
+    });
+
+    return await this.productRepository.save(newProduct)
   }
 
-  findAll() {
-    return `This action returns all products`;
+  async getAllProductsByBusiness(businessId: string) {
+    return await this.productRepository.find({
+      where: { business: { id: businessId }, isActive: true },
+      relations: ['category'],
+    });
   }
 
   findOne(id: number) {
     return `This action returns a #${id} product`;
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  async updateProduct(productId: string, updateProductDto: UpdateProductDto, file?: Express.Multer.File) {
+    const product = await this.productRepository.findOne({
+      where: { id: productId },
+      relations: ['business', 'category'],
+    });
+
+    if (!product) throw new NotFoundException('Product not found');
+
+    if (file) {
+      const uploadResult = await this.cloudinaryService.updateProductImage(file, productId)
+      updateProductDto['img'] = uploadResult.secure_url;
+    };
+
+    if (updateProductDto.category) {
+      let category = await this.categoriesProductRepository.findOne({
+        where: { name: updateProductDto.category, business: product.business },
+      });
+
+      if (!category) {
+        category = this.categoriesProductRepository.create({
+          name: updateProductDto.category,
+          business: product.business,
+        });
+
+        await this.categoriesProductRepository.save(category);
+      }
+
+      product.category = category;
+    }
+
+    Object.assign(product, updateProductDto);
+
+    return await this.productRepository.save(product);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async deleteProduct(productId: string) {
+    const product = await this.productRepository.findOne({
+      where: { id: productId },
+    });
+
+    if (!product) throw new NotFoundException('Product not found');
+
+    product.isActive = false;
+
+    await this.productRepository.save(product);
+
+    return "Product deleted successfully";
   }
 }
