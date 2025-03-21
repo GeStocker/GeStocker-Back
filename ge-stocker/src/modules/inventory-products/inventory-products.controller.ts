458206b471c8385f925912cfb88fdb83fc1387da
Buffalopeter <pedroleone95@gmail.com>
2025-03-21 14:24:50 -0300

    inventory-products: created CRUD. Post, Get and Put for Price. Soft delete managed from Business Products

--- ge-stocker/src/modules/inventory-products/inventory-products.controller.ts
+++ ge-stocker/src/modules/inventory-products/inventory-products.controller.ts
@@ -1,7 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseUUIDPipe, Post, Put, UseGuards } from '@nestjs/common';
 import { InventoryProductsService } from './inventory-products.service';
+import { AuthGuard } from '@nestjs/passport';
+import { CreateInventoryProductsDto } from './dto/create-inventory-product.dto';
+import { UpdatePriceDto, UpdateStockProductBatchDto } from './dto/update-inventory-product.dto';
 
 @Controller('inventory-products')
 export class InventoryProductsController {
   constructor(private readonly inventoryProductsService: InventoryProductsService) {}
+
+  @Post()
+  @UseGuards(AuthGuard)
+  addProductsToInventory(
+    @Body() createInventoryProductsDto: CreateInventoryProductsDto
+  ) {
+    const inventoryId = createInventoryProductsDto.inventoryId;
+    const products = createInventoryProductsDto.products;
+    return this.inventoryProductsService.addProductsToInventory(inventoryId, products)
+  };
+
+  @Get(':inventoryId')
+  @UseGuards(AuthGuard)
+  getAllInventoryProducts(@Param('inventoryId', ParseUUIDPipe) inventoryId: string) {
+    return this.inventoryProductsService.getAllInventoryProducts(inventoryId);
+  };
+
+  @Put('price/:id')
+  @UseGuards(AuthGuard)
+  updatePrice(
+    @Param('id') inventoryProductId: string,
+    @Body() UpdatePriceDto: UpdatePriceDto,
+  ) {
+    return this.inventoryProductsService.updatePrice(inventoryProductId, UpdatePriceDto)
+  };
 }
--- ge-stocker/src/modules/inventory-products/inventory-products.service.ts
+++ ge-stocker/src/modules/inventory-products/inventory-products.service.ts
@@ -1,4 +1,58 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { InventoryProduct } from './entities/inventory-products.entity';
+import { Repository } from 'typeorm';
+import { Product } from '../products/entities/product.entity';
+import { Inventory } from '../inventory/entities/inventory.entity';
+import { CreateInventoryProductsDto, InventoryProductDataDto } from './dto/create-inventory-product.dto';
+import { UpdatePriceDto, UpdateStockProductBatchDto } from './dto/update-inventory-product.dto';
 
 @Injectable()
-export class InventoryProductsService {}
+export class InventoryProductsService {
+    constructor(
+        @InjectRepository(InventoryProduct)
+        private readonly inventoryProductRepository: Repository<InventoryProduct>,
+        @InjectRepository(Product)
+        private readonly productRepository: Repository<Product>,
+        @InjectRepository(Inventory)
+        private readonly inventoryRepository: Repository<Inventory>,
+    ) {}
+
+    async addProductsToInventory(inventoryId: string, products: InventoryProductDataDto[]) {
+        const inventory = await this.inventoryRepository.findOne({
+            where: { id: inventoryId },
+        });
+
+        if (!inventory) throw new NotFoundException('Inventory not found');
+
+        const newInventoryProducts = products.map((product) => {
+            return this.inventoryProductRepository.create({
+                inventory,
+                product: { id: product.productId },
+                price: product.price,
+                stock: product.stock,
+            });
+        });
+
+        return await this.inventoryProductRepository.save(newInventoryProducts);
+    };
+
+    async getAllInventoryProducts(inventoryId: string) {
+        return await this.inventoryProductRepository.find({
+            where: { inventory: { id: inventoryId } },
+            relations: ['product'],
+        });
+    };
+
+    async updatePrice(inventoryProductId: string, updatePriceDto: UpdatePriceDto) {
+        const inventoryProduct = await this.inventoryProductRepository.findOne({
+            where: { id: inventoryProductId },
+        });
+
+        if (!inventoryProduct) throw new NotFoundException('Inventory Product not found');
+
+        inventoryProduct.price = updatePriceDto.price;
+
+        return await this.inventoryProductRepository.save(inventoryProduct);
+    };
+}
