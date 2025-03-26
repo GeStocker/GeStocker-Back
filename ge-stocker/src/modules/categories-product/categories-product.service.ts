622a39fa38820c7c8d01c062a13796c7542deee5
Tomas36M <tomasmunevar36@gmail.com>
2025-03-26 13:33:12 -0500

    feature/categories

--- ge-stocker/src/modules/categories-product/categories-product.service.ts
+++ ge-stocker/src/modules/categories-product/categories-product.service.ts
@@ -1,27 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateCategoriesProductDto } from './dto/create-categories-product.dto';
 import { UpdateCategoriesProductDto } from './dto/update-categories-product.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CategoriesProduct } from './entities/categories-product.entity';
 import { Repository } from 'typeorm';
+import { Business } from '../bussines/entities/bussines.entity';
 
 @Injectable()
 export class CategoriesProductService {
   constructor(
-    @InjectRepository(CategoriesProduct)
-    private readonly categoriesProductRepository: Repository<CategoriesProduct>,
+    @InjectRepository(CategoriesProduct) private readonly categoriesProductRepository: Repository<CategoriesProduct>,
+    @InjectRepository(Business) private readonly businessRepository: Repository<Business>
   ) {}
-  async createCategory(createCategoriesProductDto: CreateCategoriesProductDto, businessId: string): Promise<CategoriesProduct> {
+  async createCategory(
+    createCategoriesProductDto: CreateCategoriesProductDto,
+    businessId: string
+  ): Promise<CategoriesProduct> {
     const { name } = createCategoriesProductDto;
+  
+    const businessExists = await this.businessRepository.existsBy({ id: businessId });
+    if (!businessExists) {
+      throw new NotFoundException(`Business with id ${businessId} not found`);
+    }
+  
     const existingCategory = await this.categoriesProductRepository.findOne({
-      where: { name, business: { id: businessId } },
+      where: {
+        name,
+        business: { id: businessId }
+      },
+      relations: ['business']
     });
-
-    if (existingCategory) return existingCategory;
-
-    const category = this.categoriesProductRepository.create({ name, business: { id: businessId } });
-
-    return await this.categoriesProductRepository.save(category);
+  
+    if (existingCategory) {
+      throw new NotFoundException(`Category with name ${name} already exists in business with id ${businessId}`);
+    }
+  
+    const category = this.categoriesProductRepository.create({
+      name,
+      business: { id: businessId }
+    });
+  
+    return this.categoriesProductRepository.save(category);
   }
 
   async getAllCategories(businessId: string) {
