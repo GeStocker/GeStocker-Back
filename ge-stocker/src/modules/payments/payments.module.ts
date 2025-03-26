f40e04d21f781537a44602819d36d39f4e2adf4a
Tomas36M <tomasmunevar36@gmail.com>
2025-03-26 18:44:01 -0500

    feature/payments services

--- ge-stocker/src/modules/payments/payments.module.ts
+++ ge-stocker/src/modules/payments/payments.module.ts
@@ -3,9 +3,10 @@ import { PaymentsService } from './payments.service';
 import { PaymentsController } from './payments.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../users/entities/user.entity';
+import { Payment } from './entities/payment.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, Payment])],
   controllers: [PaymentsController],
   providers: [PaymentsService],
 })
