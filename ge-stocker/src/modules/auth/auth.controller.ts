15d81b93f330c2c79625d18caa0c8dfaf031a419
Tomas36M <tomasmunevar36@gmail.com>
2025-03-25 20:01:15 -0500

    backend deployment ajustes

--- ge-stocker/src/modules/auth/auth.controller.ts
+++ ge-stocker/src/modules/auth/auth.controller.ts
@@ -28,6 +28,6 @@ async googleAuthRedirect(@Req() req: CustomRequest, @Res() res) {
   await this.authService.registerOrUpdateGoogleUser(profile);
   const loginResponse = await this.authService.loginWithGoogle(profile);
   res.cookie('token', loginResponse.token, { httpOnly: false });
-  return res.redirect('http://localhost:3001/dashboard/perfil');
+  return res.redirect('https://gestocker-back.onrender.com/dashboard/perfil');
 }
 }
