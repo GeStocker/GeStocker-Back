01ef29cbbafef31186fe05e4683e803659d783e8
GregorioVilla <GregoVilla02@gmail.com>
2025-03-27 11:27:43 -0300

    Guards

--- ge-stocker/src/modules/collaborators/collaborators.controller.ts
+++ ge-stocker/src/modules/collaborators/collaborators.controller.ts
@@ -6,9 +6,11 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { CollaboratorsService } from './collaborators.service';
 import { CreateCollaboratorDto } from './dto/create-collaborator.dto';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('collaborators')
 export class CollaboratorsController {
@@ -25,15 +27,18 @@ export class CollaboratorsController {
   }
 
   @Get()
+  @UseGuards(AuthGuard)
   findAll() {
     return this.collaboratorsService.findAll();
   }
   @Get(':id')
+  @UseGuards(AuthGuard)
   findOne(@Param('id') id: string) {
     return this.collaboratorsService.findOne(id);
   }
 
   @Delete(':id')
+  @UseGuards(AuthGuard)
   remove(@Param('id') id: string) {
     return this.collaboratorsService.deactivateCollaborator(id);
   }
