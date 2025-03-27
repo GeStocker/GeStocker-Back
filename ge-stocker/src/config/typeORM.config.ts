fc1f3b28843886a772f0dcecf16ef2233cf7065a
Tomas36M <tomasmunevar36@gmail.com>
2025-03-27 14:21:07 -0500

    feature/payments

--- ge-stocker/src/config/typeORM.config.ts
+++ ge-stocker/src/config/typeORM.config.ts
@@ -7,6 +7,7 @@ import { IncomingProduct } from 'src/modules/incoming-shipment/entities/incoming
 import { IncomingShipment } from 'src/modules/incoming-shipment/entities/incoming-shipment.entity';
 import { InventoryProduct } from 'src/modules/inventory-products/entities/inventory-products.entity';
 import { Inventory } from 'src/modules/inventory/entities/inventory.entity';
+import { Payment } from 'src/modules/payments/entities/payment.entity';
 import { Product } from 'src/modules/products/entities/product.entity';
 import { User } from 'src/modules/users/entities/user.entity';
 
@@ -19,7 +20,7 @@ export const TypeOrmConfig = TypeOrmModule.forRootAsync({
         username: configService.get('DB_USER'),
         password: configService.get('DB_PASSWORD'),
         database: configService.get('DB_NAME'),
-        entities: [User, Business, Inventory, Product, CategoriesProduct, InventoryProduct, IncomingShipment, IncomingProduct, Collaborator],
+        entities: [User, Business, Inventory, Product, CategoriesProduct, InventoryProduct, IncomingShipment, IncomingProduct, Collaborator, Payment],
         synchronize: true,
         logging: false,
         // dropSchema: true,
