7417cb7213fc511ca33ca0d44ebbfac5519d62a5
Tomas36M <tomasmunevar36@gmail.com>
2025-03-27 12:07:19 -0500

    feature/deployment prueba google

--- ge-stocker/src/main.ts
+++ ge-stocker/src/main.ts
@@ -4,31 +4,41 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { LoggerMiddleware } from './middlewares/logger.middleware';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
+import { NestExpressApplication } from '@nestjs/platform-express'; // Importación agregada
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  // Especificamos el tipo NestExpressApplication
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
+  // Configuración del proxy de confianza
+  const expressApp = app.getHttpAdapter().getInstance();
+  expressApp.set('trust proxy', 1);
+
+  // Configuración de Swagger
   const swaggerConfig = new DocumentBuilder()
     .setTitle('Gestocker')
     .setDescription('API para gestionar inventarios')
     .setVersion('1.0.0')  
     .addBearerAuth()
-    .build();2
+    .build();
 
-    const document = SwaggerModule.createDocument(app, swaggerConfig);
-    SwaggerModule.setup('docs', app, document);
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('docs', app, document);
 
-    app.enableCors({
-      origin: ['http://localhost:3001', 'https://ge-stocker.vercel.app'],
-      methods: 'GET, PUT, POST, DELETE, PATCH',
-      allowedHeaders: ['Content-Type', 'Authorization'],
-      credentials: true,
-    });
+  // Configuración CORS actualizada
+  app.enableCors({
+    origin: ['http://localhost:3001', 'https://ge-stocker.vercel.app'],
+    methods: 'GET, PUT, POST, DELETE, PATCH',
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
 
+  // Middlewares
   const logger = new LoggerMiddleware();
-  app.use(cookieParser())
-  app.useGlobalPipes(new ValidationPipe({whitelist: true}));
+  app.use(cookieParser());
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.use(logger.use);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
