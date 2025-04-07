3f37358cbe52942ee7278c838d6d41a3ffc8b0d5
Tomas36M <tomasmunevar36@gmail.com>
2025-04-07 14:00:11 -0500

    feature/collaborators cambios

--- ge-stocker/src/modules/collaborators/collaborators.service.ts
+++ ge-stocker/src/modules/collaborators/collaborators.service.ts
@@ -66,7 +66,7 @@ export class CollaboratorsService {
 
     const collaboratorPayload = {
       username: collaborator.username,
-      sub: collaborator.id,
+      userId: collaborator.id,
       inventoryId: collaborator.inventory.id,
       roles: collaborator.isAdmin
         ? [UserRole.BUSINESS_ADMIN]
